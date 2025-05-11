f713bb9154a71ba1f6ebf56d7d0409ab5f33986d
Anton Tananaev <anton.tananaev@gmail.com>
2025-05-11 12:24:58 -0700

    Update namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,7 @@ val backgroundGeolocation = project(":flutter_background_geolocation")
 apply { from("${backgroundGeolocation.projectDir}/background_geolocation.gradle") }
 
 android {
-    namespace = "com.example.traccar_client"
+    namespace = "org.traccar.client"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
