43932dfbc70e182e9b6a75a81f26a9e08ab8e469
Pradul MT <pradulmt@gmail.com>
2026-06-23 09:55:53 +0530

    Rebranting for iOS: display name, appId, icon name etc done

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -173,7 +173,7 @@ class _MainScreenState extends State<MainScreen> with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Traccar Client'),
+        title: const Text('Nexemble Client'),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
